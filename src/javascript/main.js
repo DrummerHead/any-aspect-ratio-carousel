4e2c7f1871f6da1878c40d45e7e8728aa9ed1d82
DrummerHead <drummerhead@gmail.com>
2016-09-26 16:21:58 -0400

    Allow for properties to be set on element via data attributes

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,11 +3,29 @@
 
 import AarCarousel from './AarCarousel';
 
-const initCarousels = (opts) => {
+const getInlineAttributes = (element) => {
+  const data = element.dataset;
+  const result = {};
+
+  if ({}.hasOwnProperty.call(data, 'height')) {
+    result.height = data.height;
+  }
+  if ({}.hasOwnProperty.call(data, 'slideTransitionDuration')) {
+    result.slideTransitionDuration = parseInt(data.slideTransitionDuration, 10);
+  }
+  if ({}.hasOwnProperty.call(data, 'imagePanning')) {
+    result.imagePanning = true;
+  }
+
+  return result;
+};
+
+const initCarousels = (options) => {
   const carousels = document.querySelectorAll('.aar-carousel');
 
   for (const carousel of carousels) {
-    (new AarCarousel(carousel, opts)).buildUI();
+    const mergedOptions = Object.assign({}, options, getInlineAttributes(carousel));
+    (new AarCarousel(carousel, mergedOptions)).buildUI();
   }
 };
 
